ea27b66cf9f890cc14612c685cf4457747c53abd
chrisjarn <83709354+chrisjarn@users.noreply.github.com>
2024-10-11 12:10:26 +0800

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -32,10 +32,10 @@ export default function Home() {
             </div>
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
           </button>
-          <div className="lg:text-8xl text-5xl mx-auto text-center font-normal tracking-tighter  text-muted-foreground">
-            <div className="text-start mx-auto ml-20 flex justify-center">
+          <div className="lg:text-8xl text-5xl mx-auto  lg:text-center text-start font-normal tracking-tighter  text-muted-foreground">
+            <div className="text-start mx-auto lg:ml-20 px-5 lg:px-0 flex justify-center">
               <div className="">Build</div>
-              <FlipWords words={words} className="w-[500px]" /> <br />
+              <FlipWords words={words} className="lg:w-[500px] w-full" /> <br />
             </div>
             routes with{" "}
             <span className="text-primary  font-medium tracking-[-0.08em]">
