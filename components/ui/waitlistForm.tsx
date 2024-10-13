0f987d9becd0d488489ab4fd64ec7813e4555495
chrisjarn <83709354+chrisjarn@users.noreply.github.com>
2024-10-13 14:17:02 +0800

    changes

--- components/ui/waitlistForm.tsx
+++ components/ui/waitlistForm.tsx
@@ -11,12 +11,12 @@ export function SignupFormDemo() {
     console.log("Form submitted");
   };
   return (
-    <div className="max-w-xl w-full mx-auto rounded-none md:rounded-2xl p-2 md:p-0 ">
+    <div className="max-w-xl w-full mx-auto rounded-none md:rounded-2xl p-1 md:p-0 ">
       <HyperText
-        className="text-4xl font-bold text-black dark:text-white"
+        className="text-3xl font-bold text-black dark:text-white"
         text="Welcome to Routi"
       />
-      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+      <p className="text-neutral-600 text-sm max-w-sm mt-0 dark:text-neutral-300">
         Join our waitlist to be the first to try our product and receive the
         latest updates and information.
       </p>
@@ -52,14 +52,14 @@ export function SignupFormDemo() {
           />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
-            <Label htmlFor="lastname">Company Name</Label>
-            <Input
-              id="company"
-              placeholder="Routi Pty Ltd"
-              type="text"
-              className="bg-muted/30"
-            />
-          </LabelInputContainer>
+          <Label htmlFor="lastname">Company Name</Label>
+          <Input
+            id="company"
+            placeholder="Routi Pty Ltd"
+            type="text"
+            className="bg-muted/30"
+          />
+        </LabelInputContainer>
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
