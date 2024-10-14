250bddef0235dcbc8c684b66391dd06c6998baf3
chrisjarn <83709354+chrisjarn@users.noreply.github.com>
2024-10-14 18:01:16 +0800

    changes

--- components/ui/waitlistForm.tsx
+++ components/ui/waitlistForm.tsx
@@ -55,8 +55,8 @@ export function SignupFormDemo() {
 
       {/* The form element uses react-hook-form */}
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="my-6 space-y-6">
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="my-6 space-y-2">
+          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-0">
             <FormField
               control={form.control}
               name="firstname"
@@ -140,13 +140,15 @@ export function SignupFormDemo() {
               </FormItem>
             )}
           />
-          <button
-            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-            type="submit"
-          >
-            Join Waitlist &rarr;
-            <BottomGradient />
-          </button>
+          <div className="pt-2">
+            <button
+              className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+              type="submit"
+            >
+              Join Waitlist &rarr;
+              <BottomGradient />
+            </button>
+          </div>
         </form>
       </Form>
     </div>
